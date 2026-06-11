f3d058c491bb60e20c47915988837f8130146ee0
aaradhya <aaradhyasisodiya9@gmail.com>
2026-06-11 15:31:56 +0530

    Update API base URL to production Render backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://vss-backend-lsgc.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
